d902daf34e7be6f888badb385593120fd1396eed
Johan Johansson <johanj4@kth.se>
2014-11-03 22:37:27 +0100

    Enables calibration of ir sensors

--- src/ir_calibration.cpp
+++ src/ir_calibration.cpp
@@ -1,57 +1,91 @@
 #include <ros/ros.h>
 #include <ras_arduino_msgs/ADConverter.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
 
 class IrCalibrator {
 
 public:
-    IrCalibrator() : number(50) {}
+    ros::Subscriber adc_sub;
+    ros::NodeHandle nh;
+
+    IrCalibrator() : number(50) {
+        nh = ros::NodeHandle();
+        adc_sub = nh.subscribe("/arduino/adc", 1, &IrCalibrator::update, this);
+        memset(average_1, 0, sizeof(average_1));
+        memset(average_4, 0, sizeof(average_4));
+        index = 0;
+    }
+
     ~IrCalibrator() {}
 
-    void update(const ras_arduino_msgs::ADConverter& msg) {
-        average_1 += msg->ch1;
-        average_4 += msg->ch4;
+    void update(const ras_arduino_msgs::ADConverterConstPtr& msg) {
+        average_1[index] += msg->ch1;
+        average_4[index] += msg->ch4;
     }
 
     void calculateAverage() {
-        average_1 = average_1/number;
-        average_4 = average_4/number;
+        average_1[index] = average_1[index]/number;
+        average_4[index] = average_4[index]/number;
     }
 
-    void printAverage() {
-        std::cout << "Average sensor 1: " << average_1 << std::endl;
-        std::cout << "Average sensor 4: " << average_4 << std::endl;
+    void printAverage(ofstream &file) {
+        cout << "Average sensor 1: " << average_1[index] << endl;
+        cout << "Average sensor 4: " << average_4[index] << endl;
+        file << average_1[index] << ", " << average_4[index] << endl;
      }
 
+    void incrementIndex() {
+        index++;
+    }
+
+    int getIndex() {
+        return index;
+    }
+
 private:
-    int average_1;
-    int average_4;
+    int average_1[26];
+    int average_4[26];
     int number;
+    int index;
 
 };
 
 int main(int argc, char **argv) {
 
     ros::init(argc, argv, "ir_calibration");
-    ros::NodeHandle nh;
-
-    ros::Subscriber adc_sub = nh.subscribe("/arduino/adc", 1, &IrCalibrator::update, this);
-
-    ros::Rate loop_rate(10);
 
     IrCalibrator ir = IrCalibrator();
 
+    ros::Rate loop_rate(10);
+
     int number = 50;
     int iter = 0;
+    int numberOfTests = 3;
 
-    while (nh.ok()) {
-        if (iter >= number) break;
+    ofstream file("ir_values_calibration.txt");
+    file << "Sensor 1:   Sensor 4:" << endl;
+
+    while (ir.nh.ok()) {
+        if (ir.getIndex() >= numberOfTests) break;
+        if (iter >= number) {
+            cout << "Press key to start next set" << endl;
+            cin.ignore();
+            iter = 0;
+            ir.incrementIndex();
+            ir.calculateAverage();
+            ir.printAverage(file);
+        }
         iter++;
+
         ros::spinOnce();
         loop_rate.sleep();
     }
 
-    ir.calculateAverage();
-    ir.printAverage();
+    file.close();
 
     return 0;
 }
